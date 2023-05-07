693876e0769ca6cf7cc0acbb46403bb977c6c287
codeRookieErick <erickfernandomoraramirez@gmail.com>
2023-05-07 13:46:13 -0400

    Fix router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createBrowserRouter } from 'react-router-dom'
-
-import HomePage from './shared/pages/HomePage/HomePage.tsx';
-import NotFound from './shared/pages/ErrorPages/NotFound.tsx';
-import Personal from './shared/pages/Personal/Personal.tsx';
-
 import './main.css';
 import App from './App.tsx';
 
